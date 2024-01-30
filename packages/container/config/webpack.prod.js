6143be1eb1583c04f8e0548d100d6de07f3dcd88
divya <pandeydivya62@gmail.com>
2024-01-30 19:22:27 +0530

    changes for marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new moduleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packagejson.dependencies,
     }),
